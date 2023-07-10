d65c7c8bd70fe9d5e18c6c04d413edf03fe11187
Kami <harshimymommy@gmail.com>
2023-07-10 22:17:41 +0800

    test(Conn): Creating apache connection
    Porting over to apache connections soon?

---
+++ src/test/java/Connection/Apache.java
@@ -0,0 +1,23 @@
+package Connection;
+
+import org.apache.http.conn.ssl.SSLContextBuilder;
+import com.golem.skyblockutils.Main;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import logger.Logger;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+import static com.golem.skyblockutils.utils.RequestUtil.getAllowAllFactory;
+
+public class Apache {
+	public static JsonObject bazaar() throws IOException {
+
+	}
+}
