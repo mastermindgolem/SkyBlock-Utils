66b7ba41d819ee7f407278e5d39f758efdd080cd
Kami <harshimymommy@gmail.com>
2023-07-15 11:47:15 +0800

    (Logger): Logger checks

--- src/main/java/com/golem/skyblockutils/init/HelpInit.java
+++ src/main/java/com/golem/skyblockutils/init/HelpInit.java
@@ -3,6 +3,8 @@ package com.golem.skyblockutils.init;
 import com.golem.skyblockutils.command.HelpCache;
 import com.golem.skyblockutils.command.HelpInvocation;
 import com.golem.skyblockutils.command.commands.*;
+import logger.Logger;
+import net.minecraftforge.client.ClientCommandHandler;
 
 public class HelpInit {
 	public static void registerHelp() {
@@ -11,6 +13,12 @@ public class HelpInit {
 		HelpCache.addHelpProvider(EquipmentCommand.class.getName(), new EquipmentCommand());
 		HelpCache.addHelpProvider(StatCommand.class.getName(), new StatCommand());
 		HelpCache.addHelpProvider(UpgradeCommand.class.getName(), new UpgradeCommand());
+		if (ClientCommandHandler.instance.getCommands().containsKey("shelp")) {
+			Logger.info("Added Help");
+		}
+		if (ClientCommandHandler.instance.getCommands().containsKey("sbu")) {
+			Logger.info("Added SbuCommand");
+		}
 		HelpInvocation.addHelp();
 		HelpCache.addStrings();
 	}
