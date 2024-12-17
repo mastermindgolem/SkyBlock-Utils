17e44af4b45ab83df9c120d4e3e825a05305b00e
mastermindgolem <97010946+mastermindgolem@users.noreply.github.com>
2024-12-17 07:52:07 -0500

    Update Version

--- src/main/java/com/golem/skyblockutils/Main.java
+++ src/main/java/com/golem/skyblockutils/Main.java
@@ -33,7 +33,7 @@ public class Main
 	private AuctionHouse all_auctions;
 	public static JsonArray auctions = new JsonArray();
 	public static JsonObject bazaar = new JsonObject();
-	public static final String VERSION = "1.0.5.Beta.2";
+	public static final String VERSION = "1.0.5.Beta.3";
 	public static Config configFile;
 	public static GuiScreen display;
 	public static final Minecraft mc;
